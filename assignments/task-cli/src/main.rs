013ddf82619b6805553707dffef2d025b34bee01
Daniil Vassilyev <daniilvas2003@gmail.com>
2026-01-16 17:56:23 +0200

    Client implemented

--- assignments/task-cli/src/main.rs
+++ assignments/task-cli/src/main.rs
@@ -1,27 +1,46 @@
-/*  You may start from this template when implementing Task 1,
-    or use entirely own code.
- */
-
 use std::{
+    env,
     io::{Read, Write},
     net::TcpStream,
     time::Instant,
 };
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Task-CLI starting");
 
-    // Start clock to measure the time it takes do finish transmission
+    // Start clock to measure the time it takes to finish transmission
     let start = Instant::now();
 
-    /* TODO:
-        - Open TCP connection to adnet-agent server
-        - Write command message to socket: "TASK-001 keyword"
-        - Read all data incoming to the socket until the other end closes TCP connection
-        - Pay attention to total bytes read, and the last 8 characters read
-     */
+    // Creating a vector of command line arguments
+    let args: Vec<String> = env::args().collect();
+    // Checking if the number of arguments is correct
+    if args.len() != 2 {
+        eprintln!("Arguments: host:port");
+        return Err("Invalid number of arguments".into());
+    }
+    println!("{}", args[1].as_str());
 
+    // Connect to the server specified in the command line arguments
+    let mut socket = TcpStream::connect(&args[1])?;
+    // Send the TASK-CLI cheetah as the message
+    socket.write(b"TASK-CLI cheetah")?;
+
+    // Creating a vector to store all received data
+    let mut data = Vec::new();
+    // Read all data from the socket
+    socket.read_to_end(&mut data)?;
+    // println!("{:x?}", data);
+
+    // Get the total size of the received data in bytes
+    let total_size = data.len();
+    // Measure the duration taken for the transmission
     let duration = start.elapsed();
+    // Get the last 8 bytes of the received data as a string
+    let last_bytes: String = String::from_utf8_lossy(&data[total_size.saturating_sub(8)..]).to_string();
     
-    //println!("Total size: {} -- Last bytes: {} -- Duration: {:?}", total_size, last, duration);
+    // println!("{}", String::from_utf8_lossy(&data[total_size.saturating_sub(100)..]));
+
+    println!("Total size: {} -- Last bytes: {} -- Duration: {:?}", total_size, last_bytes, duration);
+
+    Ok(())
 }
